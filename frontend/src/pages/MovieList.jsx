75a382058661bb151111fdfeaaed6e58c44e4a89
Karol342 <cgonzba@gmail.com>
2026-01-12 05:36:23 +0100

    Actualizando MovieList

--- frontend/src/pages/MovieList.jsx
+++ frontend/src/pages/MovieList.jsx
@@ -26,7 +26,7 @@ const MovieList = () => {
 
             {/* Grid de Tarjetas */}
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                {movies.map((movie) => (
+                {movies?.map((movie) => (
                     <MovieCard key={movie._id} movie={movie} />
                 ))}
             </div>
